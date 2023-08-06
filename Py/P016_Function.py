ed973d231be1169c1e1f7447f82c7859b25c18a2
Nayan Patel <nayanpatel00112@gmail.com>
2023-08-06 17:37:21 +0530

    python

---
+++ Py/P016_Function.py
@@ -0,0 +1,27 @@
+def Hello():
+    print("Hello Learners!")
+
+Hello()
+Hello()
+
+def hii(a):
+    print("Hello", a)
+
+hii("Marmik")
+hii("Nayan")
+
+def sumofnum(a, b):
+    return a + b
+
+x = sumofnum(6,3)
+print(x)
+
+print(sumofnum(5, 7))
+
+# Output:
+# Hello Learners!
+# Hello Learners!
+# Hello Marmik
+# Hello Nayan
+# 9
+# 12
