a92d28bff58a36c9f8d95984373ac5921a084457
Nayan Patel <nayanpatel00112@gmail.com>
2023-08-07 13:14:58 +0530

    python

--- PY_OOPs/P025_compareobject.py
+++ PY_OOPs/P025_compareobject.py
@@ -6,7 +6,6 @@ class Humen:
     def display(self):
         print(self.name, self.age)
     
-    # ERROR May BW
     def comp(self, other): 
         if ((self.name == other.name) and (self.age == other.age)):
             return "Both are same"
@@ -15,7 +14,7 @@ class Humen:
         
 h1 = Humen("Nayan", 21)
 h2 = Humen("Jignesh", 21)
-h3 = Humen("jignesh", 21)
+h3 = Humen("Jignesh", 21)
 
 h1.display()
 h2.display()
