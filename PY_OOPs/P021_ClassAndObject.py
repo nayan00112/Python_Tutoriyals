adc8c0707f5f0af1915ad32d6ec99543a8d6f114
Nayan Patel <nayanpatel00112@gmail.com>
2023-08-07 11:32:46 +0530

    python

--- PY_OOPs/P021_ClassAndObject.py
+++ PY_OOPs/P021_ClassAndObject.py
@@ -1,1 +1,23 @@
 class Car:
+    def __init__(self, name):
+        self.name = name
+    wheel = 4
+    engineCulinder = 6
+    def run(self):
+        print("Car is running.", self.name)
+    def stop(self):
+        print("Car is stopped.", self.name)
+
+c1 = Car("BMW")
+c2 = Car("Farari")
+
+c1.run()
+c2.run()
+c1.stop()
+c2.stop()
+
+# Output:
+# Car is running. BMW
+# Car is running. Farari
+# Car is stopped. BMW
+# Car is stopped. Farari
