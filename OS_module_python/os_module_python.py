87e9d4a993b24517c12fa2c3b972092107ce7e5a
Nayan Patel <nayanpatel00112@gmail.com>
2023-09-13 22:17:45 +0530

    pandas library

---
+++ OS_module_python/os_module_python.py
@@ -0,0 +1,11 @@
+import os
+
+# os.mkdir("OS_module_python/MyFolder")
+# MyFolder folder has made.
+
+# os.rename("MyFolder", "Nayan")
+# rename folder named "MyFolder" to "Nayan"
+
+print(os.getcwd())
+
+os.chdir("")
