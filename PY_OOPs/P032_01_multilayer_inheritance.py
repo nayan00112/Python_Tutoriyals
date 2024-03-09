9ca81873794b9ac977fd480ce451c569b30ba6ec
Nayan Patel <nayanpatel00112@gmail.com>
2024-03-09 10:19:59 +0530

    multilayer inheritance

---
+++ PY_OOPs/P032_01_multilayer_inheritance.py
@@ -0,0 +1,111 @@
+class B:
+    def __init__(self):
+        print("in B super")
+class A (B):
+    def __init__(self):
+        print("in A")
+        
+aa = A()
+
+# output:
+# in A
+
+
+class C:
+    def __init__(self):
+        print("in C super")
+class D (C):
+    def __init__(self):
+        super().__init__()
+        print("in D")
+        
+dd = D()
+
+# output:
+# in C super
+# in D
+
+
+class X:
+    def __init__(self):
+        print("X super super")
+
+class Y(X):
+    def __init__(self):
+        print("in Y super")
+class Z (Y):
+    def __init__(self):
+        super().__init__()
+        print("in Z")
+        
+CC = Z()
+
+# output:
+# in Y super
+# in Z
+
+
+class AA:
+    def __init__(self):
+        print("AA super super")
+
+class BB(AA):
+    def __init__(self):
+        super().__init__()
+        print("in BB super")
+
+class CC (BB):
+    def __init__(self):
+        super().__init__()
+        print("in CC")
+        
+CCC = CC()
+
+# output:
+# in BB super
+# AA super super
+# in CC
+
+
+class MM:
+    def __init__(self):
+        print("MM super super")
+
+class NN(MM):
+    def __init__(self):
+        print("in NN super")
+        super().__init__()
+
+class OO (NN):
+    def __init__(self):
+        super().__init__()
+        print("in OO")
+        
+o = OO()
+
+# Output:
+# in NN super
+# MM super super
+# in OO
+
+
+class MMM:
+    def __init__(self):
+        print("MMM super super")
+
+class NNN(MMM):
+    def __init__(self):
+        print("in NNN super")
+        super().__init__()
+
+class OOO (NNN):
+    def __init__(self):
+        print("in OOO")
+        super().__init__()
+        
+oo = OOO()
+
+# Output:
+# in OOO
+# in NNN super
+# MMM super super
