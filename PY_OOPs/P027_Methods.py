fd4f81b976676aaecb8e5d94fad05924fe4e3db8
Nayan Patel <nayanpatel00112@gmail.com>
2024-07-29 18:50:08 +0530

    python methods

--- PY_OOPs/P027_Methods.py
+++ PY_OOPs/P027_Methods.py
@@ -38,3 +38,39 @@ Student.dispSchoolName()
 # Mitul
 # This is for students only.
 # School name is R. P. Patel School.
+
+
+
+
+# Instance Method:
+# Use: When the method operates on a specific instance of a class.
+# Access: Via the self parameter, which refers to the instance.
+# Syntax: def method_name(self, ...):
+
+# Class Method:
+# Use: When the method operates on the class itself, rather than a specific instance.
+# Access: Via the cls parameter, which refers to the class.
+# Syntax: @classmethod\ndef method_name(cls, ...):
+
+# Static Method:
+# Use: When the method operates on data or utility functions that are not dependent on the instance or the class.
+# Access: No special parameters (class or instance).
+# Syntax: @staticmethod\ndef method_name(...):
+
+
+# When to Use Each Method:
+
+# Instance Method:
+# Instance initialization and modification
+# Accessing instance attributes
+# Performing instance-specific calculations or actions
+
+# Class Method:
+# Creating new instances of the class (often called factory methods)
+# Modifying class-level attributes
+# Accessing class-level data
+
+# Static Method:
+# Utility methods that operate on data or perform calculations that are independent of the instance or class
+# Operations that relate to the class’s purpose but do not modify its state
+# Mathematical operations or conversions
